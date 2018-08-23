d0234a5541349451115cf8d3d76a97eb5b339167
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-23 19:18:57 -0400

    Initial commit for inventory lists. Updated page heading.

---
+++ inv_list/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # get 'inventories/index'
+
+  root 'inventories#index'
+  
+  get 'inventories/:id' => 'inventoires#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
